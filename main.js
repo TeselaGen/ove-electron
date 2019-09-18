cc09899928a0f40ea980b4516a362f5466b06c0a
tnrich <tnrich@teselagen.com>
2019-09-18 09:35:39 -0700

    fixing initial open

--- main.js
+++ main.js
@@ -87,7 +87,9 @@ app.on("will-finish-launching", () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow()
+});
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function() {
